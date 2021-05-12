f8d3aec76364dee133254af45c0da862c3f6bda8
Curamy <damaon2016@naver.com>
2021-05-12 13:06:38 +0900

    Save

---
+++ New Unity Project/Assets/moving.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vec = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0);
+
+        transform.Translate(vec * Time.deltaTime * 10);
+    }
+}
